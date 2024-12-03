b3ff3f633c24c1e32864ed6fae3c33992142fb29
Pablo Alcolea Sesse <128633352+PabloAlcoleaSesse@users.noreply.github.com>
2024-12-03 10:26:46 +0100

    Creación de CMakeLists.txt

---
+++ Ejercicio4/Ciencia.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Pablo Alcolea Sesse on 2/12/24.
+//
+
+#include "Ciencia.h"
+
+namespace Ciencia1 {
+    namespace Fisica {
+        double calcularEnergia(double masa) {
+            return masa * c * c;
+        }
+    }
+}
---
+++ Ejercicio4/Ciencia.h
@@ -0,0 +1,18 @@
+//
+// Created by Pablo Alcolea Sesse on 2/12/24.
+//
+
+#ifndef CIENCIA_H
+#define CIENCIA_H
+
+
+namespace Ciencia1 {
+    namespace Fisica {
+        const double c = 299792458;
+        double calcularEnergia(double masa);
+        }
+    }
+
+
+
+#endif //CIENCIA_H
