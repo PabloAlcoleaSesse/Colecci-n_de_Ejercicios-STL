24c6e8f128fc7d538edebd06ae56a574847eb989
Pablo Alcolea Sesse <128633352+PabloAlcoleaSesse@users.noreply.github.com>
2024-12-04 10:58:10 +0100

    Creación de CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 // Created by Pablo Alcolea Sesse on 4/12/24.
 //
 
-
+#include <iostream>
 #include "Ejercicio1/Matematicas.h"
 
 #include "Ejercicio3/Circ.h"
@@ -16,16 +16,28 @@
 
 
 int main() {
-    // Ejercicio 1
-    ejercicio1();
-    // Ejercicio 2
-    ejercicio2();
-    //Ejercicio 3
-    ejercicio3();
-    //Ejercicio 4
-    ejercicio4();
-    //Ejercicio 5
-    ejercicio5();
+    int opcion;
+    std::cout << "Introduce el numero del ejercicio que quieres ejecutar: ";
+    std::cin >> opcion;
+
+    switch (opcion) {
+        case 1: {
+            ejercicio1();
+            break;
+        }case 2: {
+            ejercicio2();
+            break;
+        }case 3: {
+            ejercicio3();
+            break;
+        }case 4: {
+            ejercicio4();
+            break;
+        }case 5: {
+            ejercicio5();
+            break;
+        }default: { std::cout << "Ejercicio no encontrado" << std::endl; }
+    }
 
     return 0;
 }
